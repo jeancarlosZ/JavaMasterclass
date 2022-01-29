3a45be56409314c7209ce2e8c4935a8db7fe6e35
Jean Carlos Zhong <64503277+jeancarlosZ@users.noreply.github.com>
2022-01-29 10:11:41 -0500

    feat: finished learning about keywords and expressions

---
+++ KeywordsAndExpressions/src/academy/learnprogramming/Main.java
@@ -0,0 +1,20 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // a mile is equal to 1.609344 kilometres
+        double kilometres = (100 * 1.609344);
+        int highScore = 50;
+
+        if (highScore == 50) {
+            System.out.println("This is an expression");
+        }
+
+        int score = 100; // Expression = score = 100
+        if (score > 99) { // Expression = score > 99
+            System.out.println("You got the high score!"); // Expression = "You go the high score!"
+            score = 0; // Expression = score = 0
+        }
+    }
+}
