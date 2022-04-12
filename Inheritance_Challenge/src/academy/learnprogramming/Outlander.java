3435c2ad876a1ebe7dc503bd7f1db6a53f3923cb
Jean Carlos Zhong <64503277+jeancarlosZ@users.noreply.github.com>
2022-04-11 23:35:40 -0400

    feat: inheritance challenge part 1

---
+++ Inheritance_Challenge/src/academy/learnprogramming/Outlander.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming;
+
+public class Outlander extends Car {
+    private int roadServiceMonths;
+
+    public Outlander(int roadServiceMonths) {
+        super("Outlander", "4WD", 5, 5, 6, false);
+        this.roadServiceMonths = roadServiceMonths;
+    }
+
+    public void accelerate(int rate) {
+
+    }
+}
